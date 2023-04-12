13a71013dbcc216b7da32b01d76f686a95800962
GraceGenuardi <graziagenuardi96@gmail.com>
2023-04-12 17:14:20 +0200

    EDIT aggiunti Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,37 +8,17 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class Train extends Model
 {
-    use HasApiTokens, HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'azienda', 'stazione_partenza', 'stazione_arrivo', 'orario_partenza', 'orario_arrivo', 'codice_treno', 'numero_carrozze', 'in_orario', 'cancellato'
     ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    
+    public function scopeRecentlyDeparted($query)
+    {
+        return $query->whereDate('orario_partenza', '>=', now()->toDateString())
+                     ->where('cancellato', false)
+                     ->where('in_orario', true)
+                     ->orderBy('orario_partenza', 'asc');
+    }
 }
